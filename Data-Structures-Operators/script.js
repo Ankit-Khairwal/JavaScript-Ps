f96ff7cd1370652cd7a813d55ac18c0e4aa4e304
Assigments <khairwalankit7@gmail.com>
2025-01-13 22:26:25 +0530

    added

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -308,3 +308,77 @@
 // // console.log(question.entries());
 // console.log([...question.keys()]);
 // console.log([...question.values()]);
+
+
+// Maps: Fundamentals
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
+
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open :D')
+//   .set(false, 'We are closed :(');
+
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// const time = 8;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// // rest.clear();
+
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
+// console.log(rest.size);
+
+// console.log(rest.get(arr));
+
+// // New Operations to Make Sets Useful!
+
+// const italianFoods = new Set([
+//   'pasta',
+//   'gnocchi',
+//   'tomatoes',
+//   'olive oil',
+//   'garlic',
+//   'basil',
+// ]);
+
+// const mexicanFoods = new Set([
+//   'tortillas',
+//   'beans',
+//   'rice',
+//   'tomatoes',
+//   'avocado',
+//   'garlic',
+// ]);
+
+// const commonFoods = italianFoods.intersection(mexicanFoods);
+// console.log('Intersection:', commonFoods);
+// console.log([...commonFoods]);
+
+// const italianMexicanFusion = italianFoods.union(mexicanFoods);
+// console.log('Union:', italianMexicanFusion);
+
+// console.log([...new Set([...italianFoods, ...mexicanFoods])]);
+
+// const uniqueItalianFoods = italianFoods.difference(mexicanFoods);
+// console.log('Difference italian', uniqueItalianFoods);
+
+// const uniqueMexicanFoods = mexicanFoods.difference(italianFoods);
+// console.log('Difference mexican', uniqueMexicanFoods);
+
+// const uniqueItalianAndMexicanFoods =
+//   italianFoods.symmetricDifference(mexicanFoods);
+// console.log(uniqueItalianAndMexicanFoods);
+
+// console.log(italianFoods.isDisjointFrom(mexicanFoods));
