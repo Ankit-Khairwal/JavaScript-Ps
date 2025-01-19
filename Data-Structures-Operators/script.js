ccf111c3b8f38626ed0a36dfbb72b24bab01bd30
Assigments <khairwalankit7@gmail.com>
2025-01-19 22:42:42 +0530

    new added

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -382,3 +382,71 @@
 // console.log(uniqueItalianAndMexicanFoods);
 
 // console.log(italianFoods.isDisjointFrom(mexicanFoods));
+
+/*
+// Sets
+const ordersSet = new Set([
+    'Pasta',
+    'Pizza',
+    'Pizza',
+    'Risotto',
+    'Pasta',
+    'Pizza',
+  ]);
+  console.log(ordersSet);
+
+  console.log(new Set('Ankit'));
+
+  console.log(ordersSet.size);
+  console.log(ordersSet.has('Pizza'));
+  console.log(ordersSet.has('Bread'));
+  ordersSet.add('Garlic Bread');
+  ordersSet.add('Garlic Bread');
+  ordersSet.delete('Risotto');
+  // ordersSet.clear();
+  console.log(ordersSet);
+
+  for (const order of ordersSet) console.log(order);
+
+  // Example
+  const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+  const staffUnique = [...new Set(staff)];
+  console.log(staffUnique);
+
+  console.log(
+    new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+  );
+
+  console.log(new Set('Ankit').size);
+  */
+
+// Coding Challenge #2
+/*
+// 1.
+for (const [i, player] of game.scored.entries())
+  console.log(`Goal ${i + 1}: ${player}`);
+
+// 2.
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average);
+
+// 3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
+
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+
+// BONUS
+// So the solution is to loop over the array, and add the array elements as object properties, and then increase the count as we encounter a new occurence of a certain element
+const scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+*/
