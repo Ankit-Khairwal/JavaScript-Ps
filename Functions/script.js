d771c601828d74de2c1ef3e88da77c9ea658639f
Assigments <khairwalankit7@gmail.com>
2025-02-06 23:50:52 +0530

    new

--- Functions/script.js
+++ Functions/script.js
@@ -279,4 +279,46 @@ runOnce();
   const isPrivate = 23;
   var notPrivate = 46;
 }
+  // console.log(isPrivate);
+console.log(notPrivate);
+
+
+///////////////////////////////////////
+// Closures
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+
+
+///////////////////////////////////////
+// More Closure Examples
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
 */
